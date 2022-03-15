c45a7254eb995bb37d56109b8668dc146b5b894b
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-03-15 19:47:45 -0300

    iniciando

---
+++ exercicio01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fernando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello world");
+        }
+    }
+}
